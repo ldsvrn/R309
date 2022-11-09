486f2158b47aa79be3dbf09efcdc1c9b1e5bc6bb
ldsvrn <louis2555@orange.fr>
2022-11-09 22:05:44 +0100

    ex2 exceptions

---
+++ exceptions/ex2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def printfile(file):
+    try:
+        with open(file, 'r') as f:
+            for l in f:
+                print(l.rstrip("\n\r"))  # pour virer les CRLF
+    except FileNotFoundError:
+        print("Erreur: Le fichier spécifié n'existe pas")
+    except PermissionError:
+        print("Erreur: Vous n'avez pas la permission de lire ce fichier")
+    except IOError:
+        print("Erreur: Impossible de lire le fichier")
+    finally:
+        print("Fin du programme")
+    # FileExistsError
+
+
+if __name__ == "__main__":
+    try:
+        printfile(sys.argv[1])
+    except IndexError as err:
+        print("Erreur: Aucun fichier en argument")
+        # raise Exception("Aucun fichier en argument!") from err # err cause cette exception
