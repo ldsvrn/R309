cd52cb3027a4169dbb14359f42d56c1e30c4af36
ldsvrn <louis2555@orange.fr>
2022-11-15 23:10:28 +0100

    synchrone

--- socket/synchrone/server.py
+++ socket/synchrone/server.py
@@ -15,49 +15,54 @@ connections = []
 
 
 def main():
-    server = socket.socket()
-    server.bind(HOST)
-    server.listen(5)
+    try:
+        server = socket.socket()
+        server.bind(HOST)
+        server.listen(5)
 
-    
-    msgcl = ""
-    msgsrv = ""
-    while ARRET not in (msgcl, msgsrv):
-        conn, address = server.accept()
-        if conn not in connections:
-            logging.info(f"Connected to {address}")
-            connections.append(conn)
         
         msgcl = ""
         msgsrv = ""
+        while ARRET not in (msgcl, msgsrv):
+            conn, address = server.accept()
+            if conn not in connections:
+                logging.info(f"Connected to {address}")
+                connections.append(conn)
+            
+            msgcl = ""
+            msgsrv = ""
 
-        while BYE not in (msgcl, msgsrv) and ARRET not in (msgcl, msgsrv):
-            try:
-                msgcl = conn.recv(1024).decode()
-                print(msgcl)
-                if str(msgcl) == BYE or str(msgcl) == ARRET:
+            while BYE not in (msgcl, msgsrv) and ARRET not in (msgcl, msgsrv):
+                try:
+                    msgcl = conn.recv(1024).decode()
+                    print(msgcl)
+                    if str(msgcl) == BYE or str(msgcl) == ARRET:
+                        break
+                    msgsrv = input("Message:")
+                    conn.send(msgsrv.encode())
+                except BrokenPipeError:
+                    logging.error(f"BrokenPipeError with {address}")
                     break
-                msgsrv = input("Message:")
-                conn.send(msgsrv.encode())
-            except BrokenPipeError:
-                logging.error(f"BrokenPipeError with {address}")
-                break
-        
-        if str(msgcl) == BYE or str(msgsrv) == BYE:
-            logging.info(f"Closing connection with {address}...")
-            conn.send(BYE.encode())
+            
+            if str(msgcl) == BYE or str(msgsrv) == BYE:
+                logging.info(f"Closing connection with {address}...")
+                conn.send(BYE.encode())
+                conn.close()
 
 
-    # Quand ARRET est envoyé (normalement jespere plz help)
-    logging.info("Stopping server...")
-    try:
-        conn.send(ARRET.encode())
-    except BrokenPipeError: # On s'en fout de l'erreur ici honnetement, elle arrive quand le serv veut l'arret car le client est déjà déco...
-        pass
-    for i in connections: # il y a moyen que ça pose problème plus tard car on déco déjà une conn avant
-        i.close()
-    server.close()
-    sys.exit(0)
+        # Quand ARRET est envoyé (normalement jespere plz help)
+        logging.info("Stopping server...")
+        try:
+            conn.send(ARRET.encode())
+        except BrokenPipeError: # On s'en fout de l'erreur ici honnetement, elle arrive quand le serv veut l'arret car le client est déjà déco...
+            pass
+        for i in connections: # il y a moyen que ça pose problème plus tard car on déco déjà une conn avant
+            i.close()
+        server.close()
+        sys.exit(0)
+    except PermissionError:
+        print("Le port n'est pas utilisable!")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
